9611d4574d170ebcaaca58677e259865ce266995
Matthew Chu <Mchu08@uoguelph.ca>
2025-10-01 13:14:35 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 fib = 0
 while (True):
-  num = input("User input: ")
-  print("\n")
-  if (num >= 0):
-    if (num ==o):
-      return 0
-      break
-    elif (num == 1):
-      return 1
-      break
-    else:
-      return (fib(num-2)+fib(num-1))'
-      break
-  else:
-    print("Expected output: "Please enter a positive integer."")
+  num = int(input("User input: "))
+  if num >= 0:
+    break
+  else: print("Expected output: Please enter a positive integer.")
+
+if num == 0:
+  print("0")
+if num == 1:
+  print("1")
+
+first, second = 0, 1
+print(first, end=" ")
+print(second, end=" ")
+for i in range(2, num):
+        total = first + second
+        print(total, end=" ")
+        first, second = second, total
 
 
 # Fibonacci Sequence Exercise
